b49ea41af6331b51a805bb77581a7ca45b87a875
Makan <40735596+makannew@users.noreply.github.com>
2019-08-04 23:43:52 +0800

    physic contact material added

--- js/mgame/mg.physic.settings.js
+++ js/mgame/mg.physic.settings.js
@@ -1,6 +1,6 @@
 
-
 export function cannonSettingsBuilder(result){
+
   result.physicSettings = {};
   result.cannon = initializeCannonJs();
 
@@ -10,19 +10,40 @@ export function cannonSettingsBuilder(result){
   result.physicSettings.gravity = {x:0, y:-70 , z:0};
 
   result.physicSettings.materials = {};
-  result.physicSettings.materials.groundMaterial= new CANNON.Material();
-  result.physicSettings.materials.objectMaterial= new CANNON.Material();
-  result.physicSettings.addFunction(createContactMaterial);
-  result.physicSettings.newContactMaterial= {material1: "groundMaterial" , material2: "objectMaterial" , friction: 0.3, restitution: 0.4 }
+  result.physicSettings.contactMaterials = [];
+  result.physicSettings.addFunction(buildContactMaterial);
 
-  result.physicSettings.materials.wheelMaterial= new CANNON.Material();
-  result.physicSettings.newContactMaterial= {material1: "groundMaterial" , material2: "wheelMaterial" , friction: 0.3, restitution: 0 , contactEquationStiffness: 1000}
+  result.physicSettings.materials.groundMaterial= new CANNON.Material({name:"groundMaterial"});
+  result.physicSettings.materials.objectMaterial= new CANNON.Material({name:"objectMaterial"});
+  result.physicSettings.materials.wheelMaterial= new CANNON.Material({name:"wheelMaterial"});
 
-  result.physicSettings.newContactMaterial= {material1: "objectMaterial" , material2: "wheelMaterial" , friction: 0.3, restitution: 0 ,contactEquationStiffness: 1000}
-
-
+  result.physicSettings.contactMaterials[0]={material1: "groundMaterial" , material2: "objectMaterial" , friction: 0.3, restitution: 0.4 };
+  result.physicSettings.contactMaterials[1]={material1: "groundMaterial" , material2: "wheelMaterial" , friction: 0.3, restitution: 0 , contactEquationStiffness: 1000};
+  result.physicSettings.contactMaterials[2]={material1: "objectMaterial" , material2: "wheelMaterial" , friction: 0.3, restitution: 0 ,contactEquationStiffness: 1000};
 }
 
+function buildContactMaterial({contactMaterials}){
+  for (let item of contactMaterials){
+    if (!item.added){
+      let material1 = materials[item.material1];
+      let material2 = materials[item.material2];
+      let contactEquationStiffness = 1000;
+      let contactEquationRelaxation = 3;
+      let frictionEquationRelaxation = 3;
+      let frictionEquationStiffness = 10000000;
+      if (item.contactEquationStiffness) contactEquationStiffness=item.contactEquationStiffness;
+      if (item.contactEquationRelaxation) contactEquationRelaxation=item.contactEquationRelaxation;
+      if (item.frictionEquationRelaxation) frictionEquationRelaxation=item.frictionEquationRelaxation;
+      if (item.frictionEquationStiffness) frictionEquationStiffness=item.frictionEquationStiffness;
+      material1.name=item.material1;
+      material2.name=item.material2;
+
+      cannon.addContactMaterial( new CANNON.ContactMaterial(material1 , material2 , { friction: item.friction, frictionEquationStiffness,
+        restitution: item.restitution,contactEquationStiffness,contactEquationRelaxation,frictionEquationRelaxation }) );
+      item.added = true;
+    }
+  }
+}
 const initializeCannonJs = function(){
   let result = new CANNON.World();
   result.broadphase = new CANNON.NaiveBroadphase();
@@ -33,15 +54,6 @@ const initializeCannonJs = function(){
   //result.defaultContactMaterial.friction = 0.2;
   return result;
 }
-
-const createContactMaterial = function({newContactMaterial}){
-    let material1 = newContactMaterial.material1;
-    let material2 = newContactMaterial.material2;
-    materials[material1].name = material1;
-    materials[material2].name = material2;
-    cannon.addContactMaterial( new CANNON.ContactMaterial(materials[material1] , materials[material2] , { friction: newContactMaterial.friction, restitution: newContactMaterial.restitution }) );
-    return true;
-  }
 
 const setGravity = function({gravity , cannon}){
   cannon.gravity.set (gravity.x , gravity.y , gravity.z);
